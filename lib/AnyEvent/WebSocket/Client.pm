d4b5028e05005328d7527e38b2b88f806b532d9c
Graham Ollis <plicease@cpan.org>
2014-08-04 13:02:19 -0400

    better formatting

--- lib/AnyEvent/WebSocket/Client.pm
+++ lib/AnyEvent/WebSocket/Client.pm
@@ -198,14 +198,14 @@ Patches are encouraged to improve it.
 
 If you see warnings like this:
 
- Class::MOP::load_class is deprecated at /home/ollisg/.perlbrew/libs/perl-5.18.2c34@dev/lib/perl5/x86_64-linux/Class/MOP.pm line 71.
- Class::MOP::load_class("Crypt::Random::Source::Weak::devurandom") called at /home/ollisg/.perlbrew/libs/perl-5.18.2c34@dev/lib/perl5/Crypt/Random/Source/Factory.pm line 137
+ Class::MOP::load_class is deprecated at .../Class/MOP.pm line 71.
+ Class::MOP::load_class("Crypt::Random::Source::Weak::devurandom") called at .../Crypt/Random/Source/Factory.pm line 137
  ...
 
 The problem is in the optional L<Crypt::Random::Source> module, and has
 been reported here:
 
-https://rt.cpan.org/Ticket/Display.html?id=93163&results=822cf3902026ad4a64ae94b0175207d6
+L<https://rt.cpan.org/Ticket/Display.html?id=93163&results=822cf3902026ad4a64ae94b0175207d6>
 
 You can use the patch provided there to silence the warnings.
 
